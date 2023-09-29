813469872b5fc04088ad8e3491487483bdc9ca59
BlueTachi <143790223+BlueTachi@users.noreply.github.com>
2023-09-29 15:59:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// JavaScript for a responsive navigation menu
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+});
+
+// JavaScript for smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 100, // Adjust for header height
+        behavior: 'smooth'
+      });
+    }
+  });
+});
